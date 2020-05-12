fc2b1b079182823744df82140ada73d57ce76a66
kjagriti <kjagriti@amazon.com>
2020-05-12 16:38:20 -0700

    Add FizzBuzz Logic

---
+++ EasyBasicProblems/src/com.practice.misc/FizzBuzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<>();
+        for (int i = 1;i<=n;i++){
+            if(i%3 == 0 && i%5 ==0) {
+                output.add("FizzBuzz");
+            }
+            else if(i%3 == 0){
+                output.add("Fizz");
+            }else if(i%5 == 0) {
+                output.add("Buzz");
+            } else (i%3 != 0 && i%5 !=0) {
+                output.add(Integer.toString(i));
+            } 
+        }
+        return output;
+    }
+}
+
+Time: O(N)
+Space: O(1)
